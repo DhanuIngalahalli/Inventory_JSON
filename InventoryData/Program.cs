5435e28b5e64ff5e388140637fe7b23816dfc030
sonupooja <ingalahallipooja@gmail.com>
2021-09-16 19:36:25 +0530

    welcome to Inventory Data Program

---
+++ InventoryData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Inventory Data");
+        }
+    }
+}
